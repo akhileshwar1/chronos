85d0afdd5bd71646a936c48a3240e895d3bb7ef3
Akhil Kandi <akhilprm999@gmail.com>
2025-09-15 17:29:32 +0530

    Fix state error and run it on reliance.

--- bin/ma.ml
+++ bin/ma.ml
@@ -8,7 +8,7 @@ open Chronos.Ma_crossover_strategy.Ma_crossover_strategy
 
 let () =
   Lwt_main.run (
-    let filename = "historical_data.csv" in
+    let filename = "historical_data_reliance.csv" in
     let config : (Chronos.Ma_crossover_strategy.Ma_crossover_strategy.local_config, 'local_state) Chronos_core.Strategy.config = {
       Chronos_core.Strategy.data_layer_uri = "ws://127.0.0.1:8000/candles/stream";
       oms_layer_uri = "http://localhost:9000/order/place";
--- lib/ma_crossover_strategy.ml
+++ lib/ma_crossover_strategy.ml
@@ -32,7 +32,7 @@ module Ma_crossover_strategy : S = struct
       | Ok (ptime, _, _) ->
           Some (Market_data_event {
             timestamp = ptime;
-          close_price = float_of_string close
+            close_price = float_of_string close
         })
       | Error _ -> None
       end
@@ -47,11 +47,14 @@ module Ma_crossover_strategy : S = struct
     sma_5 = [];
   sma_20 = [];
   last_side = Neutral;
-  }
+          }
 
   let calculate_sma list : float =
-    let total = List.fold_left ( +. ) 0.0 list in
-    total /. float_of_int (List.length list)
+    match list with
+    | [] -> 0.0
+    | _ -> 
+        let total = List.fold_left ( +. ) 0.0 list in
+        total /. float_of_int (List.length list)
 
   let accumulate list len item =
     if List.length list < len then
@@ -59,12 +62,19 @@ module Ma_crossover_strategy : S = struct
     else (* pop the first and insert at the end *)
       match list with
       | [] -> [item]
-      | _ :: t -> t @ [item]
+      | hd :: t -> 
+          Printf.printf "accumulate: popping %.5f, pushing %.5f\n%!" hd item;
+          t @ [item]
+
+
+  let string_of_float_list lst =
+    "[" ^ (String.concat "; " (List.map (fun x -> Printf.sprintf "%.2f" x) lst)) ^ "]"
 
       (* Process the event and transform the state *)
   let on_event (state : 'local_state Strategy.state) (event : event) : 'local_state Strategy.state =
     match event with
   | Market_data_event candle ->
+      Printf.printf "RAW CANDLE: close=%.5f \n%!" candle.close_price;
       let sma_5_list = accumulate state.local_state.sma_5 5 candle.close_price in
       let sma_20_list = accumulate state.local_state.sma_20 20 candle.close_price in
       let sma5 = calculate_sma sma_5_list in
@@ -73,23 +83,27 @@ module Ma_crossover_strategy : S = struct
       let make_completed_order side= 
         Order.make_completed_order
       ~tradingsymbol:"RELIANCE"
-      ~quantity:1
+      ~quantity:10
       ~lots:0 
       ~price: candle.close_price 
       ~side: side 
       ~strategy_name:"bb" in
+      Printf.printf "sma5_list=%s sma20_list=%s\n%!"
+       (string_of_float_list sma_5_list)
+       (string_of_float_list sma_20_list);
       if List.length sma_5_list < 5 || List.length sma_20_list < 20 then
         {state with local_state = {sma_5 = sma_5_list; sma_20 = sma_20_list; last_side = last_side}}
-    else if last_side != High && sma5 > sma20 then
+      else if last_side <> High && sma5 > sma20 then
       (Printf.printf " Crossing UP! %.2f, %.2f \n%! " sma5 sma20;
       {state with pending_orders = make_completed_order Order.Buy  @ state.pending_orders;
                   local_state = {sma_5 = sma_5_list; sma_20 = sma_20_list; last_side = High}})
-      else if last_side != Low && sma5 < sma20 then
+      else if last_side <> Low && sma5 < sma20 then
         (Printf.printf " Crossing DOWN! %.2f, %.2f \n%! " sma5 sma20;
       {state with pending_orders = make_completed_order Order.Sell @ state.pending_orders;
                   local_state = {sma_5 = sma_5_list; sma_20 = sma_20_list; last_side = Low}})
     else
-      state
+      (Printf.printf "in else \n%!";
+      {state with local_state = {sma_5 = sma_5_list; sma_20 = sma_20_list; last_side = last_side}})
 
   let extract_orders (state : 'local_state Strategy.state) : Order.t list * 'local_state Strategy.state =
     let orders_to_extract = state.pending_orders in
